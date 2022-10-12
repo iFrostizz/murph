8a2f5064448f96c1b74fe43857bd5da9ac5dc017
francois <fgg67@hotmail.fr>
2022-10-12 22:52:05 +0200

    format huff

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,39 +1,49 @@
 use crate::utils::Byte;
-use revm::opcode::JUMPDEST;
+// use revm::opcode::JUMPDEST;
 
 pub fn to_huff(parsed: Vec<Vec<Byte>>) -> String {
-    let /*mut*/ huff = String::default();
+    let mut huff = String::from("#define macro MAIN() = takes(0) returns(0) {");
 
     parsed.iter().for_each(|chunk| {
+        huff.push_str("\n\u{20}\u{20}");
+
         if chunk.len() > 1 {
             // is push + hex
-            let (push, hex) = (chunk.get(0).unwrap(), chunk.get(1..).unwrap());
+            let (_, hex) = (chunk.get(0).unwrap(), chunk.get(1..).unwrap());
+
             let hex = hex
                 .iter()
                 .map(|h| match h {
                     Byte::Hex(s) => s.to_string(),
-                    _ => panic!("no"),
+                    _ => panic!("wtf?!"),
                 })
                 .collect::<String>();
 
-            dbg!(&push, &hex);
+            let mut full_hex = String::from("0x");
+            full_hex.push_str(&hex);
+
+            huff.push_str(&full_hex);
         } else {
             // is either opcode or single hex
 
             match chunk.get(0).unwrap() {
-                Byte::Hex(_h) => {}
+                Byte::Hex(h) => {
+                    huff.push_str(h);
+                }
                 Byte::OpCode(o) => {
-                    let op_str = o.0.u8();
-                    match op_str {
-                        JUMPDEST => {
-                            // dbg!("JDEST");
-                        }
-                        _ => (),
+                    let op = o.0;
+                    match op {
+                        /*JUMPDEST => {
+                            huff.push_str("JUMPDEST");
+                        }*/
+                        _ => huff.push_str(&op.as_str().to_ascii_lowercase()),
                     }
                 }
             };
         }
     });
+
+    huff.push_str("\n}");
 
     huff
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
 use clap::Parser;
+use std::{fs::File, io::Write};
 
 mod formatter;
 mod parser;
 mod utils;
 
-/// Simple program to greet a person
+/// Transpile bytecode into huff
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
+    /// Bytecode
+    #[clap(short, long)]
     bytecode: String,
+
+    #[clap(short, long)]
+    file: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
     let bytecode = args.bytecode;
 
+    let bytecode = if let Some(stripped) = bytecode.strip_prefix("0x") {
+        stripped.to_string()
+    } else {
+        bytecode
+    };
+
     let parsed = parser::parse(bytecode);
 
     let huff = formatter::to_huff(parsed);
 
-    println!("{:#?}", &huff);
+    if let Some(path) = args.file {
+        let mut file = File::create(path).unwrap();
+        file.write_all(huff.as_bytes()).unwrap();
+    } else {
+        println!("{}", &huff);
+    }
 }

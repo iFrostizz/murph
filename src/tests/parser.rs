1554514430e7c66494be90335ae3ac3d20df596a
franfran <fgg67@hotmail.fr>
2022-10-14 13:40:04 +0200

    simplestore

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -140,36 +140,173 @@ mod parser_test {
 
     #[test]
     fn test_simple_store() {
-        let code = String::from("60348060093d393df360003560e01c8063552410771461001c5780632096525514610023575b6004356000555b60005460005260206000f360006000fd");
+        let code = String::from("60003560e01c8063552410771461001c5780632096525514610023575b6004356000555b60005460005260206000f3");
         let out = parser::parse(code);
         let (parsed, jump_table) = (out.sb, out.jt);
 
-        /*assert_eq!(
+        assert_eq!(
             parsed,
             vec![
                 SourceByte {
                     byte: vec![
                         Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
-                        Byte::Hex(String::from("03")),
+                        Byte::Hex(String::from("00")),
                     ],
-                    pc: 1
+                    pc: 0
                 },
                 SourceByte {
-                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::JUMP)))],
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::CALLDATALOAD)))],
+                    pc: 2
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("e0")),
+                    ],
                     pc: 3
                 },
                 SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::SHR)))],
+                    pc: 5
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::DUP1)))],
+                    pc: 6
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH4))),
+                        Byte::Hex(String::from("55")),
+                        Byte::Hex(String::from("24")),
+                        Byte::Hex(String::from("10")),
+                        Byte::Hex(String::from("77")),
+                    ],
+                    pc: 7
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::EQ)))],
+                    pc: 12
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH2))),
+                        Byte::Hex(String::from("00")),
+                        Byte::Hex(String::from("1c")),
+                    ],
+                    pc: 13
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::JUMPI)))],
+                    pc: 16
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::DUP1)))],
+                    pc: 17
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH4))),
+                        Byte::Hex(String::from("20")),
+                        Byte::Hex(String::from("96")),
+                        Byte::Hex(String::from("52")),
+                        Byte::Hex(String::from("55")),
+                    ],
+                    pc: 18
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::EQ)))],
+                    pc: 23
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH2))),
+                        Byte::Hex(String::from("00")),
+                        Byte::Hex(String::from("23")),
+                    ],
+                    pc: 24
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::JUMPI)))],
+                    pc: 27
+                },
+                SourceByte {
                     byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::JUMPDEST)))],
-                    pc: 4
-                }
+                    pc: 28
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("04")),
+                    ],
+                    pc: 29
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::CALLDATALOAD)))],
+                    pc: 31
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("00")),
+                    ],
+                    pc: 32
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::SSTORE)))],
+                    pc: 34
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::JUMPDEST)))],
+                    pc: 35
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("00")),
+                    ],
+                    pc: 36
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::SLOAD)))],
+                    pc: 38
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("00")),
+                    ],
+                    pc: 39
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::MSTORE)))],
+                    pc: 41
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("20")),
+                    ],
+                    pc: 42
+                },
+                SourceByte {
+                    byte: vec![
+                        Byte::Op(OpCode(ROpCode::try_from_u8(opcode::PUSH1))),
+                        Byte::Hex(String::from("00")),
+                    ],
+                    pc: 44
+                },
+                SourceByte {
+                    byte: vec![Byte::Op(OpCode(ROpCode::try_from_u8(opcode::RETURN)))],
+                    pc: 46
+                },
             ]
-        )*/
+        );
 
         assert_eq!(
             jump_table,
             JumpTable {
-                jump: HashMap::from([(25, 28), (36, 35)]),
-                jumpdest: HashSet::from([37, 44])
+                jump: HashMap::from([(16, 28), (27, 35)]),
+                jumpdest: HashSet::from([28, 35])
             }
         )
     }
